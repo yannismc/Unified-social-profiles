73c9da38dfa2f20f903c0f08ea46074a1ce82968
yannismc <ioannisaxao@mail.ntua.gr>
2020-11-18 20:01:41 +0100

    database flask app

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from tweepy import OAuthHandler, API
-from flask import Flask,jsonify,g
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask,jsonify,_app_ctx_stack
 from json import load
 import sqlite3
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, relationship, scoped_session
+from sqlalchemy import create_engine,Column,ForeignKey,Integer,String,BigInteger,Unicode, DateTime, Float
+from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
-
-# Load credentials from json file
-with open("twitter_credentials.json", "r") as file:
-    creds = load(file)
 
 @app.route('/')
 def welcome():
@@ -18,11 +14,36 @@ def welcome():
 
 @app.route('/twitter/user/<int:username>')
 def profile(username):
-    api = tweepy()
+    api = twitter_api()
 
     public_tweets = api.user_timeline(username)
     for tweet in public_tweets:
-        for tweet_ in [tweet,tweet.quoted_status,tweet.retweeted_status]:
+        user_id = tweet.user.id
+        name = tweet.user.name
+        screen_name = tweet.user.name
+        location = tweet.user.location
+        description = tweet.user.description
+        followers = tweet.user.followers_count
+        friends = tweet.user.friends_count
+        favorites = tweet.user.favourites_count
+        tweets_count = tweet.user.statuses_count
+        check_user(user_id,name,screen_name,location,description,friends,followers,favorites,tweets_count)
+        tweetid = tweet.id
+        text = tweet.text
+        created_time = tweet.created_at
+        source = tweet.source
+        quoted_id = tweet.quoted_status_id
+        retweeted_id = tweet.retweeted_status
+        language = tweet.lang
+        location = tweet.place.full_name +" "+ tweet.place.country
+        latitude = tweet.coordinates.coordinates[2]
+        longitude = tweet.coordinates.coordinates[1]
+        retweets = tweet.retweetcount
+        favorites = tweet.favorite_count
+        check_tweet(tweet_id,text,created_time,source,quoted_id,retweeted_id,language,location,latitude,longitude,retweets,favorites)
+
+        if  hasattr('tweet', 'quoted_status'):
+            tweet_= tweet.quoted_status
             user_id = tweet_.user.id
             name = tweet_.user.name
             screen_name = tweet_.user.name
@@ -30,10 +51,10 @@ def profile(username):
             description = tweet_.user.description
             followers = tweet_.user.followers_count
             friends = tweet_.user.friends_count
-            favourites = tweet_.user.favourites_count
-            tweets_count = tweet_.user.statuses_count
-            check_user(user_id,name,screen_name,location,description,friends,followers,favourites,tweets_count)
-            tweet_id = tweet_.id
+            favorites = tweet_.user.favourites_count
+            tweet_s_count = tweet_.user.statuses_count
+            check_user(user_id,name,screen_name,location,description,friends,followers,favorites,tweets_count)
+            tweetid = tweet_.id
             text = tweet_.text
             created_time = tweet_.created_at
             source = tweet_.source
@@ -43,81 +64,116 @@ def profile(username):
             location = tweet_.place.full_name +" "+ tweet_.place.country
             latitude = tweet_.coordinates.coordinates[2]
             longitude = tweet_.coordinates.coordinates[1]
-            retweets = tweet_.retweet_count
+            retweets = tweet_.retweetcount
+            favorites = tweet_.favorite_count
+            check_tweet(tweet_id,text,created_time,source,quoted_id,retweeted_id,language,location,latitude,longitude,retweets,favorites)
+        if hasattr('tweet', 'retweeted_status'):
+            tweet_= tweet.retweeted_status
+            user_id = tweet_.user.id
+            name = tweet_.user.name
+            screen_name = tweet_.user.name
+            location = tweet_.user.location
+            description = tweet_.user.description
+            followers = tweet_.user.followers_count
+            friends = tweet_.user.friends_count
+            favorites = tweet_.user.favourites_count
+            tweet_s_count = tweet_.user.statuses_count
+            check_user(user_id,name,screen_name,location,description,friends,followers,favorites,tweets_count)
+            tweetid = tweet_.id
+            text = tweet_.text
+            created_time = tweet_.created_at
+            source = tweet_.source
+            quoted_id = tweet_.quoted_status_id
+            retweeted_id = tweet_.retweeted_status
+            language = tweet_.lang
+            location = tweet_.place.full_name +" "+ tweet_.place.country
+            latitude = tweet_.coordinates.coordinates[2]
+            longitude = tweet_.coordinates.coordinates[1]
+            retweets = tweet_.retweetcount
             favorites = tweet_.favorite_count
             check_tweet(tweet_id,text,created_time,source,quoted_id,retweeted_id,language,location,latitude,longitude,retweets,favorites)
     response = [tweet._json for tweet in public_tweets]
     return jsonify(response)
 
-def tweepy():
-    auth = tweepy.OAuthHandler(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+def twitter_api():
+    # Load credentials from json file
+    with open("twitter_credentials.json", "r") as file:
+        creds = load(file)
+    auth = OAuthHandler(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
     auth.set_access_token(creds['ACCESS_TOKEN'], creds['ACCESS_SECRET'])
     return API(auth)
 
-db = SQLAlchemy(app)
+SQLALCHEMY_DATABASE_URL= "sqlite:///./twitter.sqlite3"
 
-class Tweet(db.Model):
+engine = create_engine(
+SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread":False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine)
+Base = declarative_base()
+
+db = SessionLocal()
+db_session = scoped_session(SessionLocal,scopefunc=_app_ctx_stack.__ident_func__)
+
+class Tweet(Base):
     __tablename__='tweets'
-    id = db.Column(db.BigInteger, primary_key=True)
-    text = db.Column(db.Unicode(320),nullable=False)
-    created_time = db.Column(db.DateTime,nullable=False)
-    source = db.Column(db.String(50))
-    # entities_id = db.Column(db.Integer, db.ForeignKey('entities.id'))
-    # entities = db.relationship('Entity', back_populates='entities')
+    id = Column(BigInteger, primary_key=True)
+    text = Column(Unicode(320),nullable=False)
+    created_time = Column(DateTime,nullable=False)
+    source = Column(String(50))
+    # entities_id = Column(Integer, ForeignKey('entities.id'))
+    # entities = relationship('Entity', back_populates='entities')
     # to change to three objects : mentions, hashtags and urls
     # and to corresponding intermediate tables
-    user_id = db.Column(db.BigInteger, db.ForeignKey('users.id'),nullable=False)
-    user = db.relationship('User', back_populates='tweets')
-    quoted_id = db.Column(db.BigInteger, db.ForeignKey('tweets.id'))
-    quoted = db.relationship('Tweet', back_populates='tweets')
-    retweeted_id = db.Column(db.BigInteger, db.ForeignKey('tweets.id'))
-    retweeted = db.relationship('Tweet', back_populates='tweets')
-    language = db.Column(db.String(36))
-    location = db.Column(db.String(200))
-    latitude = db.Column(db.Float)
-    longitude = db.Column(db.Float)
-    retweets = db.Column(db.Integer)
-    favorites = db.Column(db.Integer)
+    user_id = Column(BigInteger, ForeignKey('users.id'),nullable=False)
+    user = relationship('User', back_populates='tweets')
+    quoted_id = Column(BigInteger, ForeignKey('tweets.id'))
+    # quoted = relationship('Tweet', back_populates='quoted_id')
+    retweeted_id = Column(BigInteger, ForeignKey('tweets.id'))
+    # retweeted = relationship('Tweet', back_populates='retweeted_id')
+    language = Column(String(36))
+    location = Column(String(200))
+    latitude = Column(Float)
+    longitude = Column(Float)
+    retweets = Column(Integer)
+    favorites = Column(Integer)
 
-class User(db.Model):
+class User(Base):
     __tablename__='users'
-    id = db.Column(db.BigInteger, primary_key=True)
-    screen_name = db.Column(db.String(15),unique=True,nullable=False)
-    name = db.Column(db.String(50))
-    description = db.Column(db.Unicode(200))
-    tweets = db.relationship('Tweet', back_populates='users')
-    language = db.Column(db.String(36))
-    location = db.Column(db.String(200))
-    followers = db.Column(db.Integer)
-    friends = db.Column(db.Integer)
-    favorites = db.Column(db.Integer)
-    tweets_count = db.Column(db.Integer)
+    id = Column(BigInteger, primary_key=True)
+    screen_name = Column(String(15),unique=True,nullable=False)
+    name = Column(String(50))
+    description = Column(Unicode(200))
+    tweets = relationship('Tweet', back_populates='user')
+    language = Column(String(36))
+    location = Column(String(200))
+    followers = Column(Integer)
+    friends = Column(Integer)
+    favorites = Column(Integer)
+    tweets_count = Column(Integer)
 
-db.create_all()
 
-engine = create_engine('sqlite:///twitter.sqlite3')
-Session = sessionmaker(bind=engine)
-session = Session()
+Base.metadata.create_all(bind=engine)
+Base.query = db_session.query_property()
 
 def add_item(item):
-    db.session.add(item)
-    db.session.commmit()
+    db_session.add(item)
+    db_session.commmit()
 
-def check_user(userid,name,screen_name,location,description,friends,followers,favourites,tweets_count):
-    if userid not in [user.id for user in User.query.all()]:
-        user = User(id=userid,name=name,screen_name=screen_name,location=location,description=description,friends=friends,followers=followers,favourites=favourites,tweets_count=tweets_count)
+def check_user(userid,name,screen_name,location,description,friends,followers,favorites,tweets_count):
+    if userid not in User.query.all():
+        user = User(id=userid,name=name,screen_name=screen_name,location=location,description=description,friends=friends,followers=followers,favorites=favorites,tweets_count=tweets_count)
         add_item(user)
 
 def check_tweet(tweet_id,text,created_time,source,quoted_id,retweeted_id,language,location,latitude,longitude,retweets,favorites):
-    if tweet_id not in [tweet.id for tweet in Tweet.query.all()]:
-        tweet = Twwet(id=tweet_id,text=text,created_time=created_time,source=source,quoted_id=quoted_id,retweeted_id=retweeted_id,language=language,location=location,latitude=latitude,longitude=longitude,retweets=retweets,favorites=favorites)
+    if tweet_id not in Tweet.query.all():
+        tweet = Tweet(id=tweet_id,text=text,created_time=created_time,source=source,quoted_id=quoted_id,retweeted_id=retweeted_id,language=language,location=location,latitude=latitude,longitude=longitude,retweets=retweets,favorites=favorites)
         add_item(tweet)
 
 if __name__ == '__main__':
     app.run()
 
+    
 @app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db.session is not None:
-        db.session.close()
+def teardown_db(exception):
+    db.close()
